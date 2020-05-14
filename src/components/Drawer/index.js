011dc0336a173f154295c611b3055af45b982b3c
Jeffrey Luong <jjck1988@gmail.com>
2020-05-14 16:47:35 -0400

    Some style tweaks

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -20,13 +20,13 @@ const Drawer = ({ className, children, onClose }) => {
     <div className={styles.DrawerContainer}>
       <div className={cx(styles.DrawerBg, { [styles.displayBg]: shouldOpen })} onClick={onCloseDrawer} />
       <aside className={cx(styles.Drawer, className, { [styles.animateOpen]: shouldOpen })}>
-        {children}
         <button className={styles.DrawerClose} onClick={onCloseDrawer} type="button" ariaLabel="Close">
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
             <path d="M0 0h24v24H0z" fill="none" />
             <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" fill={Colors.primaryTheme} />
           </svg>
         </button>
+        {children}
       </aside>
     </div>
   )
